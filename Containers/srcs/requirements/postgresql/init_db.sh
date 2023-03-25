af441edd642173c21a3a5c283d50b4467e6c3d50
NeoWaffleSpy <nathancaba.etu@outlook.fr>
2023-03-25 19:12:29 +0100

    test

--- Containers/srcs/requirements/postgresql/init_db.sh
+++ Containers/srcs/requirements/postgresql/init_db.sh
@@ -3,10 +3,15 @@
 set -e
 
 # init user and db
-psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB <<-EOSQL
+psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $POSTGRES_DB <<-EOSQL
 	CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
 	CREATE DATABASE $DB_NAME;
 	GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
 
-psql -a -f ./docker-entrypoint-initdb.d/Setup.sql -d $DB_NAME
+psql -v ON_ERROR_STOP=1 -U $POSTGRES_USER -d $DB_NAME <<-EOSQL
+	GRANT ALL ON SCHEMA public TO $DB_USER;
+	DROP DATABASE $POSTGRES_DB;
+EOSQL
+
+psql -a -f /inoxdb/Setup.sql -U $DB_USER -d $DB_NAME
