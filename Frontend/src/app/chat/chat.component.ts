0734b0cc1f289ac61447afb6ac5f229b82c01a83
tmoragli <tmoragli@student.42.fr>
2023-04-23 19:49:41 +0200

    update angular

--- Frontend/src/app/chat/chat.component.ts
+++ Frontend/src/app/chat/chat.component.ts
@@ -19,9 +19,10 @@ export class ChatComponent implements OnInit {
 	icone_list = {
 		add_friend: "https://static.vecteezy.com/system/resources/previews/020/936/584/original/add-friend-icon-for-your-website-design-logo-app-ui-free-vector.jpg",
 		block_user: "https://static.thenounproject.com/png/45218-200.png",
-		mute_user: "https://static.thenounproject.com/png/45218-200.png",
-		kick_user: "https://static.thenounproject.com/png/45218-200.png",
-		ban_user: "https://static.thenounproject.com/png/45218-200.png",
+		mute_user: "https://cdn.discordapp.com/attachments/982595990517334098/1099736272311959562/mind.png",
+		kick_user: "https://cdn.discordapp.com/attachments/982595990517334098/1099728807658790992/kick-off.png",
+		ban_user: "https://as1.ftcdn.net/v2/jpg/02/13/00/44/1000_F_213004467_Vf445gOqhpiIwRmrFQUBRUDxLuQbXw1s.jpg",
+		del_message: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnP0pzY-gE90i0RyzqOh7n2QmSmRDnjzHurbizuxk&s",
 	}
 
 	constructor(
@@ -65,22 +66,115 @@ export class ChatComponent implements OnInit {
 		}
 	}
 
+	onDropdownDel(msg: MessageDto) {
+		const delMsgElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-del');
+		if (!delMsgElm)
+			return;
+		if (msg.author.user_id == Number(localStorage.getItem('id'))) {
+			if (delMsgElm.classList.contains('show')) {
+				delMsgElm.classList.remove('show');
+				delMsgElm.removeAttribute('title');
+			}
+			else {
+				delMsgElm.classList.add('show');
+				delMsgElm.setAttribute('title', 'Delete message');
+			}
+		}
+		else {
+			this.client.emit('checkPrivileges', msg);
+			const sub = fromEvent(this.client, 'answerPrivileges').subscribe((data) => {
+				if (!data) {
+					if (delMsgElm.classList.contains('show')) {
+						delMsgElm.classList.remove('show');
+						delMsgElm.removeAttribute('title');
+					}
+					else {
+						delMsgElm.classList.add('show');
+						delMsgElm.setAttribute('title', 'Delete message');
+					}
+				}
+				sub.unsubscribe();
+			})
+		}
+	}
+
 	async  onDropdrownMessage(msg: MessageDto) {
-		console.log("response");
 		const addFriendElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-add');
 		const blockUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-block');
+		const muteUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-mute');
+		const kickUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-kick');
+		const banUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-ban');
 		if (!addFriendElm)
 			return;
 		if (addFriendElm.classList.contains('show')) {
 			addFriendElm.classList.remove('show');
+			addFriendElm.removeAttribute('title');
 			blockUserElm.classList.remove('show');
-		} else {
-			addFriendElm.classList.add('show');
-			blockUserElm.classList.add('show');
+			blockUserElm.removeAttribute('title');
+			muteUserElm.classList.remove('show');
+			muteUserElm.removeAttribute('title');
+			kickUserElm.classList.remove('show');
+			kickUserElm.removeAttribute('title');
+			banUserElm.classList.remove('show');
+			banUserElm.removeAttribute('title');
 		}
-		this.client.emit('checkPrivileges', msg);
-		fromEvent(this.client, 'answerPrivileges').subscribe((data) => {
-			console.log(data);
+		else {
+			this.client.emit('checkPrivileges', msg);
+			const sub = fromEvent(this.client, 'answerPrivileges').subscribe((data) => {
+				addFriendElm.classList.add('show');
+				addFriendElm.setAttribute('title', 'Add friend');
+				blockUserElm.classList.add('show');
+				blockUserElm.setAttribute('title', 'Block User');
+				if (!data) {
+					muteUserElm.classList.add('show');
+					muteUserElm.setAttribute('title', 'Mute User');
+					kickUserElm.classList.add('show');
+					kickUserElm.setAttribute('title', 'Kick User');
+					banUserElm.classList.add('show');
+					banUserElm.setAttribute('title', 'Ban User');
+				}
+				sub.unsubscribe();
+			})
+		}
+	}
+
+	addFriend(msg: MessageDto) {}
+	blockUser(msg: MessageDto) {}
+	muteUser(msg: MessageDto) {
+		const muteUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-mute');
+		if (!muteUserElm.classList.contains('show'))
+			return;
+		this.client.emit('mute', {
+			target_id: msg.author.user_id,
+			time: 10,
+			message: "You have been muted",
+		})
+	}
+	kickUser(msg: MessageDto) {
+		const kickUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-kick');
+		if (!kickUserElm.classList.contains('show'))
+			return;
+		this.client.emit('kick', {
+			target_id: msg.author.user_id,
+			message: "You have been kicked",
+		})
+	}
+	banUser(msg: MessageDto) {
+		const banUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-ban');
+		if (!banUserElm.classList.contains('show'))
+			return;
+		this.client.emit('ban', {
+			target_id: msg.author.user_id,
+			message: "You have been banned",
+		})
+	}
+	delMsg(msg: MessageDto) {
+		const banUserElm = this.elRef.nativeElement.querySelector('#img-' + msg.message_id + '-del');
+		if (!banUserElm.classList.contains('show'))
+			return;
+		this.client.emit('ban', {
+			target_id: msg.author.user_id,
+			message: "You have been banned",
 		})
 	}
 
