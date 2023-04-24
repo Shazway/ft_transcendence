23d93c13d99f92b26b5afd2a66a56bc52f6119e3
NeoWaffleSpy <nathancaba.etu@outlook.fr>
2023-04-24 23:59:11 +0200

    wow, so smooth

--- Frontend/src/app/pong/pong.component.ts
+++ Frontend/src/app/pong/pong.component.ts
@@ -1,6 +1,8 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Socket, io } from 'socket.io-client';
 import { WebsocketService } from '../websocket.service';
+import { Application, Graphics } from 'pixi.js';
+import { Position, pongObjectDto } from 'src/dtos/Pong.dto';
 
 @Component({
   selector: 'app-pong',
@@ -8,20 +10,68 @@ import { WebsocketService } from '../websocket.service';
   styleUrls: ['./pong.component.css']
 })
 export class PongComponent {
-	client!: Socket;
-
+	private client!: Socket;
+	private app;
+	private player; 
+	
 	constructor(
 		private websocketService: WebsocketService,
-	) {}
+		private pixiContainer: ElementRef,
+		) {
+			this.app = new Application({
+				height: 600,
+				width: 1000,
+				antialias: true,
+			});
+		this.player	= new pongObjectDto(this.app.view.width, this.app.view.height);
+		this.initObjects();
+		this.pixiContainer.nativeElement.appendChild(this.app.view);
+		this.app.ticker.add((delta) => this.update(delta));
+	}
+
+
+	update(delta: number) {
+		if (this.player.inputs.ArrowUp)
+			this.player.moveObject(this.player.position(0, -3 * delta));
+		if (this.player.inputs.ArrowDown)
+			this.player.moveObject(this.player.position(0, 3 * delta));
+	}
+
+	initObjects() {
+		this.player.init(0, 0, 20, 100);
+		this.app.stage.addChild(this.player.graphic);
+		// const ruler = new Graphics();
+		// for (let index = 0; index < 12; index++) {
+		// 	for (let index2 = 0; index2 < 8; index2++) {
+		// 		ruler.beginFill(0xFF0000);
+		// 		ruler.drawRect(index * 100, index2 * 100, 2, 2);
+		// 		ruler.endFill();
+		// 	}
+		// }
+		// this.app.stage.addChild(ruler);
+	}
 
 	initSocket(match_id: number) {
 		this.client = io('ws://localhost:3005?match_id=' + match_id, this.websocketService.getHeader());
 	}
 
-	@HostListener('window:keyup', ['$event'])
+	@HostListener('window:keyup', ['$event']) 
 	handleKeyUp(event: KeyboardEvent) {
 		const key = event.key;
 		// this.client.send("on sen fout" ,JSON.stringify({ type: 'keyEvent', key }));
-		console.log(event.key);
+		if (key == 'ArrowUp')
+			this.player.inputs.ArrowUp = false;
+		if (key == 'ArrowDown')
+			this.player.inputs.ArrowDown = false;
+	}
+
+	@HostListener('window:keydown', ['$event']) 
+	handleKeyDown(event: KeyboardEvent) {
+		const key = event.key;
+		// this.client.send("on sen fout" ,JSON.stringify({ type: 'keyEvent', key }));
+		if (key == 'ArrowUp')
+			this.player.inputs.ArrowUp = true;
+		if (key == 'ArrowDown')
+			this.player.inputs.ArrowDown = true;
 	}
 }
---
+++ Frontend/src/dtos/Pong.dto.ts
@@ -0,0 +1,65 @@
+import { Graphics } from "pixi.js";
+
+export interface Position {
+	x: number;
+	y: number;
+}
+
+export class pongObjectDto {
+	public graphic = new Graphics();
+	public inputs = {
+		ArrowUp: false,
+		ArrowDown: false,
+	}
+	public gameDim: Position;
+	public objDim: Position;
+	constructor(
+		private gameWidth: number,
+		private gameHeight: number,
+		) {
+			this.gameDim = this.position(gameWidth / 2, gameHeight / 2);
+			this.objDim = this.position(this.graphic.width, this.graphic.height);
+	}
+
+	init(posX: number, posY: number, width: number, height: number) {
+		this.objDim.x = width;
+		this.objDim.y = height;
+		this.graphic.beginFill(0xFFFFFF);
+		this.graphic.drawRect(posX, posY, width, height);
+		this.graphic.endFill();
+	}
+
+	checkWallCollision(newPos: Position, playerDim: Position) {
+		if (newPos.x < 0)
+			newPos.x = 0;
+		else if (newPos.x + playerDim.x > this.gameDim.x)
+			newPos.x = this.gameDim.x - playerDim.x;
+		if (newPos.y < 0)
+			newPos.y = 0;
+		else if (newPos.y + playerDim.y > this.gameDim.y)
+			newPos.y = this.gameDim.y - playerDim.y;
+		return newPos;
+	}
+	
+	applyMove(newPos: Position, playerDim: Position) {
+		this.graphic.clear();
+		this.graphic.beginFill(0xFFFFFF);
+		this.graphic.drawRect(newPos.x, newPos.y, playerDim.x, playerDim.y);
+		this.graphic.endFill();
+		this.graphic.x = newPos.x;
+		this.graphic.y = newPos.y;
+	}
+
+	position(newX: number, newY: number) : Position {
+		return {x: newX, y: newY}
+	}
+
+	moveObject(dir: Position) {
+		let pos: Position = {x: this.graphic.x, y: this.graphic.y};
+		const width = this.objDim.x;
+		const height = this.objDim.y;
+
+		pos = this.checkWallCollision(this.position(pos.x + dir.x, pos.y + dir.y), this.position(width / 2, height / 2));
+		this.applyMove(pos, this.objDim);
+	}
+}
